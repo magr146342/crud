50ee31870d110508b70f3df9288be2b2eebb0124
manolo garcia <manologarcia1725@hotmail.com>
2021-02-21 12:28:42 -0500

    fin crud en memoria

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, {useState} from 'react'
-import { isEmpty } from 'lodash'
+import { isEmpty, size } from 'lodash'
 import shortid from 'shortid'
 
 function App() {
   const [tarea, setTarea] = useState("")
   const [tareas, setTareas] = useState([])
+  const [modoEditar, setmodoEditar] = useState(false)
+  const [id, setId] = useState("")
+  const [error, seterror] = useState(null)
 
+  const validacionform =() => {
+    let esValido = true
+    seterror(null)
+    if (isEmpty(tarea)) {
+      seterror("Debe ingresar una tarea.")
+      esValido = false
+    }
+    return esValido
+  }
+
+  //funcion agregar tarea
   const agregarTarea= (e) => {
     e.preventDefault()
-    if (isEmpty(tarea)) {
-      console.log("Tarea Vacia")
+
+    if (!validacionform()) {
       return
     }
 
@@ -22,6 +36,34 @@ function App() {
     setTarea("")
   }
 
+    //funcion guardar tarea
+    const guardarTarea= (e) => {
+      e.preventDefault()
+      if (!validacionform()) {
+        return
+      }
+      
+      const tareasEditada = tareas.map(item => item.id === id ? {id, nombre: tarea} : item)
+      setTareas(tareasEditada)
+      setmodoEditar(false)
+      setTarea("")
+      setId("")
+    }
+
+  //funcion eliminar tarea
+  const eliminarTarea=(id) => {
+    const filtrartareas = tareas.filter(tarea => tarea.id !== id)
+    setTareas(filtrartareas)
+  }
+
+  //funcion editar tarea
+  const editarTarea=(laTarea) => {
+    setTarea(laTarea.nombre)
+    setmodoEditar(true)
+    setId(laTarea.id)
+  }
+
+
   return (
     <div className="container mt-5">
       <h1>Tareas</h1>
@@ -29,23 +71,39 @@ function App() {
       <div className="row">
         <div className="col-8">
           <h4 className="text-center">Lista De Tareas</h4>
-          <ul className="list-group">
-            { 
-            tareas.map((tarea) => 
-              <li className="list-group-item" key={tarea.id}>
-              <span className="lead">{tarea.nombre}</span>
-              <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-              <button className="btn btn-warning btn-sm float-right">Editar</button>
-              </li>
+          {
+            size(tareas) == 0 ? 
+            (<li className="list-group-item">No hay tareas programadas.</li>) :
+            (
+              <ul className="list-group">
+                { 
+                  tareas.map((tarea) => 
+                    <li className="list-group-item" key={tarea.id}>
+                      <span className="lead">{tarea.nombre}</span>
+                      <button 
+                      className="btn btn-danger btn-sm float-right mx-2"
+                      onClick = {() => eliminarTarea(tarea.id)}>
+                        
+                        Eliminar</button>
+
+                      <button 
+                      className="btn btn-warning btn-sm float-right"
+                      onClick = {() => editarTarea(tarea)}>
+                        Editar</button>
+                    </li>
+                  ) 
+                }
+              </ul>
             )
-              
-            }
-          </ul>
+          }
         </div>
 
         <div className="col-4">
-          <h4 className="text-center">Formulario</h4>
-          <form onSubmit={agregarTarea}>
+          <h4 className="text-center">{modoEditar ? "Modificar Tarea" : "Agregar Tarea"}</h4>
+          <form onSubmit={modoEditar ? guardarTarea : agregarTarea}>
+            {
+            error && <span className="text-danger">{error}</span>
+            }
             <input
             type="text"
             className="form-control mb-2"
@@ -53,8 +111,8 @@ function App() {
             onChange={(texto) => setTarea(texto.target.value)}
             value={tarea}
             />
-            <button className="btn btn-dark btn-block" type="submit">
-              Agregar Tarea
+            <button className= {modoEditar ? "btn btn-warning btn-block" : "btn btn-dark btn-block"} type="submit">
+              {modoEditar ? "Editar Tarea" : "Agregar Tarea"}
             </button>
           </form>
         </div>
